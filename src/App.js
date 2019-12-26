bfcbea1fded2bc8db2b9270e780919fc9f12d7da
pedrokorb <pedro.korb@gmail.com>
2019-12-26 01:04:13 -0300

    add storage to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
+import Moment from 'react-moment';
+import 'moment-timezone';
+import { convertUnixToTimestamp } from "./Utils"
 
 function App() {
   const [location, setLocation] = useState(false);
   const [weather, setWeather] = useState(false);
+  const [lastSync, setLastSync] = useLocalStorage('lastSync', Date.now())
 
   let getWeather = async (lat, long) => {
     let key = process.env.REACT_APP_DARK_SKY_KEY
@@ -16,15 +20,42 @@ function App() {
         units: 'si'
       }
     });
-    setWeather(res.data);    
+    setWeather(res.data);
+    setLastSync(Date.now())    
   }
   console.log(weather)
+  
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       getWeather(position.coords.latitude, position.coords.longitude);
       setLocation(true)
     })
   }, [])
+
+  function useLocalStorage(key, initialValue) {
+    const [storedValue, setStoredValue] = useState(() => {
+      try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        console.log(error);
+        return initialValue;
+      }
+    });
+
+    const setValue = value => {
+      try {
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        setStoredValue(valueToStore);
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return [storedValue, setValue];
+  }
 
   if (!location) {
     return (
@@ -42,30 +73,46 @@ function App() {
     return (
       <Fragment>
         <p>
-          Temperatura do ar: 
+          Temperatura do ar:&nbsp;
           <span className="text-4xl font-bold">
-            {weather.currently.temperature} °C
+            {weather.currently.temperature.toFixed(1)} °C
           </span>
         </p>
         <p>
-          Umidade Relativa do ar: 
+          Umidade Relativa do ar:&nbsp;
           <span className="text-4xl font-bold">
-            {weather.currently.humidity}%
+            {weather.currently.humidity.toFixed(1)}%
           </span>
         </p>
         <p>
-          Velocidade do Vento: 
+          Velocidade do Vento:&nbsp;
           <span className="text-4xl font-bold">
-            {weather.currently.windSpeed}m/s
+            {weather.currently.windSpeed.toFixed(0)} m/s
           </span>
         </p>
-        <p>Volume de Chuva (ver como calcular)</p>
-        <p>Data e hora da medição: 
+        <p>Volume de Chuva:&nbsp;
           <span className="text-4xl font-bold">
-            {weather.currently.time}
+            {weather.currently.precipIntensity} mm
+          </span>
+          &nbsp;na última hora
+        </p>
+        <p>Data e hora da medição:&nbsp; 
+          <span className="text-4xl font-bold">
+            {convertUnixToTimestamp(weather.currently.time)}
           </span>
         </p>
-        <p>Data e hora da última sincronização com a API Darksky</p>
+        <p>Data e hora da última sincronização com a API Darksky:&nbsp;
+          <span className="text-4xl font-bold">
+            {convertUnixToTimestamp(lastSync/1000)}
+          </span>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={lastSync}
+          onChange={e => setLastSync(e.target.value)}
+        />
       </Fragment>
     );
   }
